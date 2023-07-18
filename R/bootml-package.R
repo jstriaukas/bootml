24738a30fc2ff96bddceb804ccf049a2d8ed7ce2
Jonas Striaukas <jonas.striaukas@gmail.com>
2023-07-18 18:40:57 +0300

    Initial commit

---
+++ R/bootml-package.R
@@ -0,0 +1,9 @@
+#' Bootstrap methods for selecting the tuning parameter for LASSO-type regression models
+#'
+#' @name bootml-package
+#' @aliases bootml
+#' @docType package
+#' @title bootml
+#' @author Jonas Striaukas (maintainer) \email{jonas.striaukas@gmail.com}
+#' @keywords package
+NULL
---
+++ README.md
@@ -0,0 +1,2 @@
+# bootml
+ Tuning-free bootstrap for testing factor regression against factor-augmented sparse alternatives
